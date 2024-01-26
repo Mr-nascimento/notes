c194ce54415ba6c06100b965e2abf1f42c46095e
Igor Nascimento <igortjnascimento@gmail.com>
2024-01-26 18:31:44 -0300

    add component section and page details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,18 +1,55 @@
 
-import {Container} from './styles'
+import {Container, Links, Content} from './styles'
 
-import { Button } from '../../components/Button'
+import { Tag } from '../../components/Tag'
 import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {  
   
   return (
     <Container>
       <Header />
-      <h1>Hello World</h1>
-      <span>Fragment</span>
 
-      <Button title="Voltar"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href='https://www.rocketseat.com.br/'>
+                  www.rocketseat.com.br
+                </a>
+              </li>
+              <li>
+                <a href='https://www.rocketseat.com.br/'>
+                  www.rocketseat.com.br
+                </a>
+              </li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="Node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
